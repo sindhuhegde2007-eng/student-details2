e88d4e6e5d06fbfb64736ea4356e54e9cc88b124
sindhuhegde2007-eng <sindhuhegde2007@gmail.com>
2025-09-30 10:03:36 +0530

    save to master

--- student.py
+++ student.py
@@ -2,9 +2,11 @@
 name = input("Enter student name: ")
 roll = input("Enter roll number: ")
 grade = input("Enter grade: ")
+branch =input("enter branch:")
 
 # Display student details
 print("\nStudent Details:")
 print("Name:", sindhu)
 print("Roll Number:", 08)
 print("Grade:", A)
+print("branch:",BCA)
