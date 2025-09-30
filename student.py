8bf9eace92009e19bd1b70beddceffcd0e3fbda5
sindhuhegde2007-eng <sindhuhegde2007@gmail.com>
2025-09-30 09:41:18 +0530

    save to main branch

---
+++ student.py
@@ -0,0 +1,10 @@
+# Collect student details
+name = input("Enter student name: ")
+roll = input("Enter roll number: ")
+grade = input("Enter grade: ")
+
+# Display student details
+print("\nStudent Details:")
+print("Name:", sindhu)
+print("Roll Number:", 08)
+print("Grade:", A)
